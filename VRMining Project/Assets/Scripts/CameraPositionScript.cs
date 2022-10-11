77b0625f447088c71da81eed7b33fa69c7561836
VeeRuby Developer <61759322+veeruby-developer@users.noreply.github.com>
2022-10-11 18:22:17 +0530

    highlight object

--- VRMining Project/Assets/Scripts/CameraPositionScript.cs
+++ VRMining Project/Assets/Scripts/CameraPositionScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CameraPositionScript : MonoBehaviour
 {
+    public GameObject welcomeCanvas;
+    GameObject benchHighlight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        benchHighlight = GameObject.FindGameObjectWithTag("Bench");
+        benchHighlight.SetActive(true);
     }
 
     // Update is called once per frame
@@ -21,6 +24,7 @@ public class CameraPositionScript : MonoBehaviour
         gameObject.SetActive(false);
         Vector3 pos = new Vector3(32.08f, -9.8f, -9.39f);
         gameObject.transform.position = pos;
+        gameObject.transform.Rotate(0, 130, 0);
         gameObject.SetActive(true);
         
     }
@@ -28,11 +32,40 @@ public class CameraPositionScript : MonoBehaviour
     public void AirView()
     {
         gameObject.SetActive(false);
-        Vector3 pos = new Vector3(-14.7f, 2.7f, -19.760f);
+        Vector3 pos = new Vector3(-97.9f, 20.2f, 18.4f);
         gameObject.transform.position = pos;
         //gameObject.transform.rotation = new Quaternion(27, -20, 0, 0);
         gameObject.GetComponent<CharacterController>().enabled = false;
         gameObject.SetActive(true);
+        welcomeCanvas.SetActive(true);
 
+        StartCoroutine(WelcomeCanvasOff());
+        
+    }
+
+    IEnumerator WelcomeCanvasOff()
+    {
+        yield return new WaitForSeconds(5);
+        welcomeCanvas.SetActive(false);
+        StartCoroutine(Bench());
+    }
+    IEnumerator Bench()
+    {
+        Debug.Log("bench");
+        yield return new WaitForSeconds(5);
+        
+        //benchHighlight.SetActive(true);
+        //Debug.Log("b");
+        gameObject.SetActive(false);
+        Vector3 pos = new Vector3(-46.6f, 17.11f, -15f);
+        gameObject.transform.position = pos;
+        gameObject.SetActive(true);
+        StartCoroutine(Crest());
+    }
+    IEnumerator Crest()
+    {
+        yield return new WaitForSeconds(5);
+        GameObject highlight = GameObject.FindGameObjectWithTag("Crest");
+        highlight.SetActive(true);
     }
 }
